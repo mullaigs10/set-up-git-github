d64e56447ef7e331fe7156d3644ae10ce6c8f9eb
Mullai Subramaniyan <mullaigs10@gmail.com>
2023-06-27 09:33:55 -0600

    Initial Commit

---
+++ newProject/newProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  newProjectApp.swift
+//  newProject
+//
+//  Created by Scholar on 6/27/23.
+//
+
+import SwiftUI
+
+@main
+struct newProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
